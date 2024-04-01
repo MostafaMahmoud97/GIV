6fe882ed7f0263808328d2ea8905790aa58727c0
MostafaMahmoud97 <48657743+MostafaMahmoud97@users.noreply.github.com>
2024-04-01 13:15:59 +0200

    Business Request

--- database/migrations/2024_04_01_081613_create_business_requests_table.php
+++ database/migrations/2024_04_01_081613_create_business_requests_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->integer("no_branches")->default(0);
             $table->string("store_type")->default("")->nullable();
             $table->string("website")->default("")->nullable();
-            $table->longText("store_address")->default("")->nullable();
+            $table->longText("store_address")->nullable();
             $table->string("first_name");
             $table->string("last_name");
             $table->string("phone_number");
